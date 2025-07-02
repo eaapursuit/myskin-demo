701da1de1c16ce942f1e049f25ba64d4340d7267
EliMacBook <ekaansah@gmail.com>
2025-07-02 16:29:00 -0400

    edited about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,10 +14,7 @@ const About = () => {
         <div className="bg-[#F8F6F6] rounded-3xl p-12 shadow-2xl">
         <p className="text-lg text-chocolate max-w-md leading-relaxed mx-auto">
           MySkin is a digital platform dedicated to helping people navigate
-          their acne journey with confidence. Designed to bridge the knowledge
-          gaps that often prolong skin struggles, MySkin connects users to niche
-          skincare communities while providing science-backed educational
-          resources. Currently, MySkin lives as a growing community on Substack,
+          their acne journey with confidence. Currently, MySkin lives as a growing community on Substack,
           where founder, Toluwani, shares daily skin journals, expert insights,
           and interviews with people across all skin types. Slated to launch as
           an app in early to mid-2026, MySkin will revolutionize the way people
